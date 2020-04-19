f61d556ac12392d21440806b7ab092ae495576f0
AlxKor <akor744@gmail.com>
2020-04-20 00:31:44 +0300

    Update lesson2_4.py

--- lesson2_4.py
+++ lesson2_4.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 my_str = input("Введите несколько слов, разделенных пробелами- ").split()
 str_num = 1
 for i in my_str:
@@ -6,4 +7,14 @@ for i in my_str:
         str_num += 1
     else:
         print(f"{str_num}) {i[0:9]}")
+=======
+my_str = input("Введите несколько слов, разделенных пробелами- ").split()
+str_num = 1
+for i in my_str:
+    if int(len(i)) <= 0:
+        print(f"{str_num}) {i}")
+        str_num += 1
+    else:
+        print(f"{str_num}) {i[0:9]}")
+>>>>>>> c95485a9c8f536e77837f9d33cb6345f40ae5bd1
         str_num += 1
