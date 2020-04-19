fb27ef252f72950577a4aa66372746ceb66c885a
AlxKor <akor744@gmail.com>
2020-04-20 00:31:46 +0300

    Update lesson2_5.py

--- lesson2_5.py
+++ lesson2_5.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 value = int(input("Введите натуральное число: "))
 my_list = [9, 7, 5, 4, 2, ]
 counter = my_list.count(value)
@@ -13,4 +14,21 @@ for el in my_list:
             break
         elif value < my_list[len(my_list)]:
             my_list.append(value)
+=======
+value = int(input("Введите натуральное число: "))
+my_list = [9, 7, 5, 4, 2, ]
+counter = my_list.count(value)
+for el in my_list:
+    if counter > 0:
+        i = my_list.index(value)
+        my_list.insert(i + counter, value)
+        break
+    else:
+        if value > el:
+            n = my_list.index(el)
+            my_list.insert(n, value)
+            break
+        elif value < my_list[len(my_list)]:
+            my_list.append(value)
+>>>>>>> c95485a9c8f536e77837f9d33cb6345f40ae5bd1
 print(my_list)
