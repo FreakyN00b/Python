6f1a2c674761d1778e34d4889b5a08e4b3823490
AlxKor <akor744@gmail.com>
2020-04-20 00:31:40 +0300

    Update lesson2_2.py

--- lesson2_2.py
+++ lesson2_2.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 my_list = ['a', 'b', 'c', 'd', 'e']
 if len(my_list) % 2 == 0:
     i = 0
@@ -13,4 +14,21 @@ else:
         my_list[i] = my_list[i + 1]
         my_list[i + 1] = el
         i += 2
+=======
+my_list = ['a', 'b', 'c', 'd', 'e']
+if len(my_list) % 2 == 0:
+    i = 0
+    while i < len(my_list):
+        el = my_list[i]
+        my_list[i] = my_list[i+1]
+        my_list[i+1] = el
+        i += 2
+else:
+    i = 0
+    while i < len(my_list) - 1:
+        el = my_list[i]
+        my_list[i] = my_list[i + 1]
+        my_list[i + 1] = el
+        i += 2
+>>>>>>> c95485a9c8f536e77837f9d33cb6345f40ae5bd1
 print(my_list)
